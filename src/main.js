b0066e570ca48dc12e3a281cd11e3a6059d09c0f
Bruno <bdorigon@hotmail.com>
2024-03-31 22:48:32 -0300

    section_faq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll("[data-tab-button]");
+  const questions = document.querySelectorAll("[data-faq-question]");
 
   for (let i = 0; i < buttons.length; i++) {
     const buttonArray = buttons[i];
@@ -15,6 +16,17 @@ document.addEventListener("DOMContentLoaded", () => {
       removeActiveButton();
 
       e.target.classList.add("shows__tabs__button--is-active");
+    });
+  }
+
+  for (let i = 0; i < questions.length; i++) {
+    const questionsArray = questions[i];
+
+    questionsArray.addEventListener("click", (element) => {
+      const classe = "faq__questions__item--is--open";
+      const elementoPai = element.target.parentNode;
+
+      elementoPai.classList.toggle(classe);
     });
   }
 });
